74b0609fadfdb8aa0904dc94ac7842189dc7bd97
Md Rehanul Haque <rehan78khan87@gmail.com>
2025-03-27 19:56:09 +0530

    corrs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors({
     methods: ["POST", "GET"],
-    origin: ["https://ze-enterprice.vercel.app/", "http://localhost:3000"]
+    origin: "*"
 }))
 
 app.use(express.json())
